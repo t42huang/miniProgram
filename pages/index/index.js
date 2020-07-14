d4e911bfa4f01e011a3ade3f0a190c3a9bae028a
Tina Huang <t42huang@gmail.com>
2020-07-14 19:55:49 -0400

    update weahter and pic automatically

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,16 +19,18 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: '',
-    forcast: [],
+    hourlyWeather: [],
+  },
+
+  onLoad(){
+    this.getNow()
   },
   onPullDownRefresh() {
     this.getNow(()=>{
       wx.stopPullDownRefresh()
     })
   },
-  onLoad(){
-    this.getNow()
-  },
+  
   getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
@@ -40,28 +42,32 @@ Page({
         let result = res.data.result
         let temp = result.now.temp
         let weather = result.now.weather
-        let nowHour = new Date().getHours()
-        let forcast = []
-        for(let i=0; i<24; i+=3){
-          forcast.push({
-            time: (i+nowHour)%24 +'时',
-            iconPath: '/images/snow-bg.png',
-            temp: '12'
-          }
-          )
-        }
-        forcast[0].time = '现在'
         this.setData({
-          nowTemp: temp,
+          nowTemp: temp + '°',
           nowWeather: weatherMap[weather],
           nowWeatherBackground: '/images/'+weather+'-bg.png',
-          forcast: forcast
         })
-
         wx.setNavigationBarColor({
           frontColor: '#000000',
           backgroundColor: weatherColorMap[weather],
         })
+        //set forcast
+        console.log(result)
+        let forecast = result.forecast
+        let nowHour = new Date().getHours()
+        let hourlyWeather = []
+        for(let i=0; i<24; i+=3){
+          hourlyWeather.push({
+            time: (i+nowHour)%24 +'时',
+            iconPath: '/images/'+forecast[i/3].weather +'-icon.png',
+            temp: forecast[i/3].temp +'°'
+          }
+          )
+        }
+        hourlyWeather[0].time = '现在'
+        this.setData({
+          hourlyWeather: hourlyWeather
+        })       
       },
       complete: ()=>{
         callback && callback()
