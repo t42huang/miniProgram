a5e3d5ad5ca16bf5f52f3f3f292765985a0b4f4b
Tina Huang <t42huang@gmail.com>
2020-07-14 02:01:26 -0400

    update data dynamically

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,23 @@
-Page({})
+Page({
+  data: {
+    nowTemp: 14,
+    nowWeather: 'cloudy'
+  },
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '广州市'
+      },
+      success (res) {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+      }
+    })
+  }
+  
+
+})
